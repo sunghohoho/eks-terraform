7c507e874407383332996e6d4eb6ed1708e0847b
sunghohoho <kim5259746@gmail.com>
2024-08-06 13:37:16 +0900

    modify 변수화 및 정책 추가

--- modules/common/external-dns.tf
+++ modules/common/external-dns.tf
@@ -1,23 +1,79 @@
+# external dns에 필요한 role, irsa 설정
+resource "aws_iam_role" "external_dns" {
+  name_prefix = substr("${var.cluster_name}-kube-system-external-dns-", 0, 37)
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "${var.oidc_provider_arn}"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "${var.oidc_issuer_url}:aud": "sts.amazonaws.com",
+          "${var.oidc_issuer_url}:sub": "system:serviceaccount:kube-system:external-dns"
+        }
+      }
+    }
+  ]
+}
+  POLICY
+
+    inline_policy {
+      name = "route53-access"
+
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ChangeResourceRecordSets"
+      ],
+      "Resource": [
+        "arn:aws:route53:::hostedzone/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ListHostedZones",
+        "route53:ListResourceRecordSets"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+    }
+}
+
+# external dns 설치
 resource "helm_release" "external-dns" {
   name = "external-dns"
   chart = "external-dns"
   repository = "https://charts.bitnami.com/bitnami"
-  version = "8.2.2"
+  version = var.external_dns_chart_version
   namespace = "kube-system"
 
-  set {
-    name = "serviceAccount.create"
-    value = "false"
-  }
-
-  set {
-    name = "serviceAccount.name"
-    value = var.exdns_service_account
-  }
-
-  set {
-    name = "rbac.create"
-    value = "true"
-  }
-
+  values = [
+    <<EOV
+serviceAccount:
+  create: true
+  annotations:
+    eks.amazonaws.com/role-arn: ${aws_iam_role.external_dns.arn}
+    meta.helm.sh/release-namespace: kube-system
+    app.kubernetes.io/managed-by: Helm
+  txtOwnerId: ${var.cluster_name}
+  policy: sync
+  extraArgs:
+  - --annotation-filter=external-dns.alphan.kubernetes.io/exclude notin (true)
+EOV
+  ]
 }
