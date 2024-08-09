5c296064894c69891b23fd9d40f8bd6e518fc301
sunghohoho <kim5259746@gmail.com>
2024-08-09 19:59:59 +0900

    add acm data block
    acm 가져오기 추가

--- dev/data.tf
+++ dev/data.tf
@@ -3,3 +3,10 @@ data "aws_region" "current" {}
 
 # 현재 리전의 가용영역 가져오기, data.aws_availability_zones.azs.names[x] 로 사용
 data "aws_availability_zones" "azs" {}
+
+# 기존 acm 가져요기, data.aws_certificate.acm.id로 사용
+data "aws_acm_certificate" "acm" {
+  domain = "gguduck.com"
+  statuses = ["ISSUED"]
+  types = ["AMAZON_ISSUED"]
+}
