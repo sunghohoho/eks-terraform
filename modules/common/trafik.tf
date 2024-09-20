510644d9d4a334e00451f1dfda54b13dd30f1c95
sunghohoho <kim5259746@gmail.com>
2024-09-20 15:42:16 +0900

    add traefik
    사용법 모름

---
+++ modules/common/trafik.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_namespace" "traefik" {
+  metadata {
+    name = "traefik"
+  }
+}
+
+resource "helm_release" "traefik" {
+  chart = "traefik"
+  name = "traefik"
+  repository = "https://traefik.github.io/charts"
+  namespace = kubernetes_namespace.traefik.metadata[0].name
+
+  values = [
+    templatefile("${path.module}/helm-values/traefik.yaml", {
+      cert_arn = var.acm_arn
+    })
+  ]
+
+  depends_on = [ helm_release.alb_controller ]
+}
