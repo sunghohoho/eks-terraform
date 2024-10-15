66c4a90e8ea94e16b950df83edad40e50edb6a2b
sunghohoho <kim5259746@gmail.com>
2024-10-15 16:52:20 +0900

    add "karpenter nodepool, class"
    add

--- dev/manifest.tf
+++ dev/manifest.tf
@@ -88,6 +88,10 @@ YAML
   depends_on = [ module.eks ]
 }
 
+################################################################################
+# The manifest verifies that Karpenter is deployed nodegroup and working.
+################################################################################
+
 resource "kubectl_manifest" "karpenter-test-deploy" {
   yaml_body = <<EOF
 apiVersion: apps/v1
@@ -117,34 +121,7 @@ spec:
             cpu: 1
         securityContext:
           allowPrivilegeEscalation: false
-}
-
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: inflate
-spec:
-  replicas: 0
-  selector:
-    matchLabels:
-      app: inflate
-  template:
-    metadata:
-      labels:
-        app: inflate
-    spec:
-      terminationGracePeriodSeconds: 0
-      securityContext:
-        runAsUser: 1000
-        runAsGroup: 3000
-        fsGroup: 2000
-      containers:
-      - name: inflate
-        image: public.ecr.aws/eks-distro/kubernetes/pause:3.7
-        resources:
-          requests:
-            cpu: 1
-        securityContext:
-          allowPrivilegeEscalation: false
 EOF
+
+depends_on = [ kubectl_manifest.nodeclasses ]
 }
