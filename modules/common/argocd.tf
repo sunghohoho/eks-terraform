26412d3f494344db8683d1126e82d86f03c06f20
sunghohoho <kim5259746@gmail.com>
2024-11-16 02:10:30 +0900

    update argocd conf
    argocd 에서 사용할 sa, ns 설정
    sa의 경우 모든 배포를 위한 admin 권한 부여 및 sectet type = sa token 발급

--- modules/common/argocd.tf
+++ modules/common/argocd.tf
@@ -1,6 +1,44 @@
-resource "kubernetes_namespace" "argocd" {
+# argocd 네임스페이스 
+resource "kubernetes_namespace_v1" "argocd" {
   metadata {
     name = "argocd"
+  }
+}
+
+# argocd 서비스 어카운트
+resource "kubernetes_service_account_v1" "argocd" {
+  metadata {
+    name      = "argocd"
+    namespace = kubernetes_namespace_v1.argocd.metadata[0].name
+  }
+}
+
+# service account에 권한을 부여할 secret 생성
+resource "kubernetes_secret_v1" "argocd" {
+  metadata {
+    generate_name = "argocd"
+    namespace     = kubernetes_namespace_v1.argocd.metadata[0].name
+    annotations = {
+      "kubernetes.io/service-account.name" = kubernetes_service_account_v1.argocd.metadata[0].name
+    }
+  }
+
+  type = "kubernetes.io/service-account-token"
+}
+
+resource "kubernetes_cluster_role_binding_v1" "argocd-sa-rolebinding" {
+    metadata {
+    name = "argocd-sa-rolebinding-admin"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account_v1.argocd.metadata[0].name
+    namespace = kubernetes_namespace_v1.argocd.metadata[0].name
   }
 }
 
@@ -13,13 +51,15 @@ resource "helm_release" "argocd" {
   chart = "argo-cd"
   name = "argo-cd"
   repository = "https://argoproj.github.io/argo-helm"
-  namespace = kubernetes_namespace.argocd.metadata[0].name
+  namespace = kubernetes_namespace_v1.argocd.metadata[0].name
   version = var.argocd-chart-version
 
   values = [
     templatefile("${path.module}/helm-values/argocd.yaml", {
       cert_arn = var.acm_arn
       server_admin_password = htpasswd_password.argocd.bcrypt
+      argocd_sa = kubernetes_service_account_v1.argocd.metadata[0].name
     })
   ]
 }
+
