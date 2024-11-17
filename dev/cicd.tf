285425a08340e4c4cebf3d676fc28e8ce3951649
sunghohoho <kim5259746@gmail.com>
2024-11-17 15:46:08 +0900

    Update cicd.tf
    기본 project application 삭제 및 project 생성 후 해당 프로젝트에 application 배포

--- dev/cicd.tf
+++ dev/cicd.tf
@@ -1,29 +1,30 @@
 # 테스트 argocd applicaiton
-resource "kubectl_manifest" "argocd_app" {
-  yaml_body =  <<EOF
-apiVersion: argoproj.io/v1alpha1
-kind: Application
-metadata:
-  name: cad
-  namespace: argocd
-spec:
-  project: default
-  source:
-    repoURL: https://github.com/sunghohoho/cats-and-dogs-helm
-    targetRevision: HEAD
-    path: .
-    helm:
-      valueFiles: 
-      - values.yaml
-  destination:
-    name: in-cluster
-    namespace: default
-  syncPolicy:
-    automated:
-      prune: true
-EOF
-}
+# resource "kubectl_manifest" "argocd_app" {
+#   yaml_body =  <<EOF
+# apiVersion: argoproj.io/v1alpha1
+# kind: Application
+# metadata:
+#   name: cad
+#   namespace: argocd
+# spec:
+#   project: default
+#   source:
+#     repoURL: https://github.com/sunghohoho/cats-and-dogs-helm
+#     targetRevision: HEAD
+#     path: .
+#     helm:
+#       valueFiles: 
+#       - values.yaml
+#   destination:
+#     name: in-cluster
+#     namespace: default
+#   syncPolicy:
+#     automated:
+#       prune: true
+# EOF
+# }
 
+# argocd cluster 등록에 사용하는 secret
 resource "kubernetes_secret_v1" "test" {
   metadata {
     name      = "test-secrets"
@@ -46,4 +47,53 @@ resource "kubernetes_secret_v1" "test" {
       }
     })
   }
+}
+
+# argocd test project
+resource "kubectl_manifest" "argocd_project" {
+  yaml_body =  <<EOF
+apiVersion: argoproj.io/v1alpha1
+kind: AppProject
+metadata: 
+  name: sample-project
+  namespace: argocd
+spec:
+  clusterResourceWhitelist:
+  - group: "*"
+    kind: "*"
+  destinations:
+  - namespace: default
+    server: https://kubernetes.default.svc
+  orphanedResources:
+    warn: false
+  sourceRepos:
+  - "*"
+  EOF
+}
+
+resource "kubectl_manifest" "argocd_app2" {
+  yaml_body =  <<EOF
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: cad-2
+  namespace: argocd
+spec:
+  project: ${kubectl_manifest.argocd_project.name}
+  source:
+    repoURL: https://github.com/sunghohoho/cats-and-dogs-helm
+    targetRevision: HEAD
+    path: .
+    helm:
+      valueFiles: 
+      - values.yaml
+  destination:
+    name: in-cluster
+    namespace: default
+  syncPolicy:
+    automated:
+      prune: true
+EOF
+
+depends_on = [ kubectl_manifest.argocd_project ]
 }
