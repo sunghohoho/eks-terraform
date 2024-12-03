4e9944b3b8c624788f76974b929eddcfb94fadcd
sunghohoho <kim5259746@gmail.com>
2024-12-03 14:30:56 +0900

    update keycloak
    키클락 모듈로 이동

--- modules/common/keycloak.tf
+++ modules/common/keycloak.tf
@@ -29,104 +29,127 @@ resource "helm_release" "keycloak" {
   ]
 }
 
-# # 렐름 설정
-# resource "keycloak_realm" "realm" {
-#   realm             = local.project
-#   enabled           = true
-#   display_name      = local.project
-#   display_name_html = "<b> 어렵다 ${local.project}</b>"
-#   user_managed_access = true
+# 렐름 설정
+resource "keycloak_realm" "realm" {
+  realm             = "dev-${var.cluster_name}-realm"
+  enabled           = true
+  display_name      = "dev-${var.cluster_name}-realm"
+  display_name_html = "<b> 어렵다 ${var.cluster_name}</b>"
+  user_managed_access = true
 
-#   login_theme = "base"
+  login_theme = "base"
+}
 
-# }
+# 렐름 내부의 사용자 생성
+resource "keycloak_user" "this" {
+  realm_id   = keycloak_realm.realm.id
+  username   = "alice"
+  enabled    = true
 
-# # 렐름 내부의 사용자 생성
-# resource "keycloak_user" "this" {
-#   realm_id   = keycloak_realm.realm.id
-#   username   = "alice"
-#   enabled    = true
+  email      = "alice@naver.com"
+  first_name = "Alice"
+  last_name  = "Aliceberg"
+  initial_password {
+    value     = jsondecode(data.aws_secretsmanager_secret_version.this.secret_string)["keycloak"]["password"]
+    temporary = false
+  }
+}
 
-#   email      = "alice@naver.com"
-#   first_name = "Alice"
-#   last_name  = "Aliceberg"
+# 그룹 생성
+resource "keycloak_group" "this" {
+  realm_id   = keycloak_realm.realm.id
+  name     = "ArgoCDAdmins"
+}
 
-#   attributes = {
-#     foo = "bar"
-#     multivalue = "value1##value2"
-#   }
+# 그룹 멤버십 생성
+resource "keycloak_user_groups" "this" {
+  realm_id   = keycloak_realm.realm.id
+  user_id   = keycloak_user.this.id
+  group_ids = [keycloak_group.this.id]
+}
 
-#   initial_password {
-#     value     = jsondecode(data.aws_secretsmanager_secret_version.this.secret_string)["keycloak"]["password"]
-#     temporary = false
-#   }
-# }
+################################################################################
+# argocd
+################################################################################
 
-# # 그룹 생성
-# resource "keycloak_group" "this" {
-#   realm_id   = keycloak_realm.realm.id
-#   name     = "ArgoCDAdmins"
-# }
+# argocd 클라이언트 
+resource "keycloak_openid_client" "argocd_client" {
+  realm_id            = keycloak_realm.realm.id
+  client_id           = "argocd"
+  name                = "argocd client"
+  enabled             = true
+  access_type         = "CONFIDENTIAL"
+  standard_flow_enabled = true
+  direct_access_grants_enabled = true
 
-# # 그룹 멤버십 생성
-# resource "keycloak_user_groups" "this" {
-#   realm_id   = keycloak_realm.realm.id
-#   user_id   = keycloak_user.this.id
-#   group_ids = [keycloak_group.this.id]
-# }
+  root_url  = "https://argocd${var.domain_name}"
+  admin_url = "https://argocd${var.domain_name}"
+  web_origins = [
+    "https://argocd${var.domain_name}"
+  ]
+  valid_redirect_uris = [
+    "https://argocd${var.domain_name}",
+    "https://argocd${var.domain_name}/auth/callback"
+  ]
+  valid_post_logout_redirect_uris = ["+"]
 
-# # argocd 클라이언트 
-# resource "keycloak_openid_client" "argocd_client" {
-#   realm_id            = keycloak_realm.realm.id
-#   client_id           = "argocd"
-#   name                = "argocd client"
-#   enabled             = true
-#   access_type         = "CONFIDENTIAL"
-#   standard_flow_enabled = true
-#   direct_access_grants_enabled = true
+  login_theme = "keycloak"
 
-#   root_url  = "https://argocd.${local.domain_name}"
-#   admin_url = "https://argocd.${local.domain_name}"
-#   web_origins = [
-#     "https://argocd.${local.domain_name}"
-#   ]
-#   valid_redirect_uris = [
-#     "https://argocd.${local.domain_name}",
-#     "https://argocd.${local.domain_name}/auth/callback"
-#   ]
-#   valid_post_logout_redirect_uris = ["+"]
+  depends_on = [ keycloak_realm.realm ]
+}
 
-#   login_theme = "keycloak"
+resource "keycloak_openid_client_scope" "argocd_client_scope" {
+  realm_id = keycloak_realm.realm.id
+  name = "groups"
+}
 
-#   depends_on = [ keycloak_realm.realm ]
-# }
+resource "keycloak_openid_group_membership_protocol_mapper" "argocd_group_membership_mapper" {
+  realm_id        = keycloak_realm.realm.id
+  client_scope_id = keycloak_openid_client_scope.argocd_client_scope.id
+  name = "groups"
 
-# resource "keycloak_openid_client_scope" "argocd_client_scope" {
-#   realm_id = keycloak_realm.realm.id
-#   # name     = "argocd-client-scope"
-#   name = "groups"
-# }
+  claim_name = keycloak_openid_client_scope.argocd_client_scope.name
+}
 
-# resource "keycloak_openid_group_membership_protocol_mapper" "argocd_group_membership_mapper" {
-#   realm_id        = keycloak_realm.realm.id
-#   client_scope_id = keycloak_openid_client_scope.argocd_client_scope.id
-#   # name            = "argocd-group-membership-mapper"
-#   name = "groups"
+resource "keycloak_openid_client_default_scopes" "argocd" {
+  realm_id  = keycloak_realm.realm.id
+  client_id = keycloak_openid_client.argocd_client.id
 
-#   claim_name = keycloak_openid_client_scope.argocd_client_scope.name
-# }
+  default_scopes = [
+    "acr",
+    "basic",
+    "profile",
+    "email",
+    "roles",
+    "web-origins",
+    keycloak_openid_client_scope.argocd_client_scope.name
+  ]
+}
 
-# resource "keycloak_openid_client_default_scopes" "argocd" {
-#   realm_id  = keycloak_realm.realm.id
-#   client_id = keycloak_openid_client.argocd_client.id
+################################################################################
+# jenkins
+################################################################################
+resource "keycloak_openid_client" "jenkins_client" {
+  realm_id            = keycloak_realm.realm.id
+  client_id           = "jenkins"
+  name                = "jenkins"
+  enabled             = true
+  access_type         = "CONFIDENTIAL"
+  standard_flow_enabled = true
+  direct_access_grants_enabled = true
 
-#   default_scopes = [
-#     "acr",
-#     "basic",
-#     "profile",
-#     "email",
-#     "roles",
-#     "web-origins",
-#     keycloak_openid_client_scope.argocd_client_scope.name
-#   ]
-# }
+  root_url  = "https://jenkins${var.domain_name}"
+  admin_url = "https://jenkins${var.domain_name}"
+  web_origins = [
+    "https://jenkins${var.domain_name}"
+  ]
+  valid_redirect_uris = [
+    "https://jenkins${var.domain_name}",
+    "https://jenkins${var.domain_name}/*"
+  ]
+  valid_post_logout_redirect_uris = ["+"]
+
+  login_theme = "keycloak"
+
+  depends_on = [ keycloak_realm.realm ]
+}
