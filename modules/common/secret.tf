e5f76757018feb805c90b0425e714afe381dc6ca
sunghohoho <kim5259746@gmail.com>
2024-12-07 01:35:23 +0900

    Update secret.tf
    안써서 비활

--- modules/common/secret.tf
+++ modules/common/secret.tf
@@ -10,49 +10,49 @@ output "secrets" {
   value = jsondecode(data.aws_secretsmanager_secret_version.this.secret_string)["jenkins"]["password"]
 }
 
-# secrets store
-resource "helm_release" "secrets-store-csi-driver" {
-  chart = "secrets-store-csi-driver"
-  name = "secrets-store-csi-driver"
-  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
-  namespace = "kube-system"
-  version = "1.4.2"
+# # secrets store
+# resource "helm_release" "secrets-store-csi-driver" {
+#   chart = "secrets-store-csi-driver"
+#   name = "secrets-store-csi-driver"
+#   repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
+#   namespace = "kube-system"
+#   version = "1.4.2"
 
-  values = [
-    <<EOF
-syncSecret:
-  enabled: true
-enableSecretRotation: false
-    EOF
-  ]
-}
+#   values = [
+#     <<EOF
+# syncSecret:
+#   enabled: true
+# enableSecretRotation: false
+#     EOF
+#   ]
+# }
 
-resource "helm_release" "secrets-store-csi-driver-provider-aws" {
-  chart = "secrets-store-csi-driver-provider-aws"
-  name = "secrets-store-csi-driver-provider-aws"
-  repository = "https://aws.github.io/secrets-store-csi-driver-provider-aws"
-  namespace = "kube-system"
-  version = "0.3.8"
-}
+# resource "helm_release" "secrets-store-csi-driver-provider-aws" {
+#   chart = "secrets-store-csi-driver-provider-aws"
+#   name = "secrets-store-csi-driver-provider-aws"
+#   repository = "https://aws.github.io/secrets-store-csi-driver-provider-aws"
+#   namespace = "kube-system"
+#   version = "0.3.8"
+# }
 
-# 해당 provider에 매칭되는 secretObjects의 data 값이 변경되는 경우 reload에 의해 pod가 재생성됩니다. (reload는 아직 미구현)
-resource "kubectl_manifest" "aws-secret-provider-class" {
-  yaml_body =  <<EOF
-apiVersion: secrets-store.csi.x-k8s.io/v1
-kind: SecretProviderClass
-metadata:
-  name: ${data.aws_secretsmanager_secret_version.this.secret_id}-class
-spec:
-  provider: aws
-  parameters:
-    objects: |
-      - objectName: ${data.aws_secretsmanager_secret_version.this.secret_id}
-        objectType: "secretsmanager"
-  EOF
+# # 해당 provider에 매칭되는 secretObjects의 data 값이 변경되는 경우 reload에 의해 pod가 재생성됩니다. (reload는 아직 미구현)
+# resource "kubectl_manifest" "aws-secret-provider-class" {
+#   yaml_body =  <<EOF
+# apiVersion: secrets-store.csi.x-k8s.io/v1
+# kind: SecretProviderClass
+# metadata:
+#   name: ${data.aws_secretsmanager_secret_version.this.secret_id}-class
+# spec:
+#   provider: aws
+#   parameters:
+#     objects: |
+#       - objectName: ${data.aws_secretsmanager_secret_version.this.secret_id}
+#         objectType: "secretsmanager"
+#   EOF
 
-  depends_on = [ 
-    helm_release.secrets-store-csi-driver, 
-    helm_release.secrets-store-csi-driver-provider-aws
-  ]
-}
+#   depends_on = [ 
+#     helm_release.secrets-store-csi-driver, 
+#     helm_release.secrets-store-csi-driver-provider-aws
+#   ]
+# }
 
