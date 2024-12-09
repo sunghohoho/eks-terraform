806c027fecb6f1ecdae5f31a44c33c3906fc5ec7
sunghohoho <kim5259746@gmail.com>
2024-12-09 10:43:10 +0900

    Create kubernetes-event.tf
    kuberentes event exporter 추가
    kibana에서 index는 자동으로 추가됨

---
+++ dev/kubernetes-event.tf
@@ -0,0 +1,71 @@
+resource "kubernetes_namespace" "kube-event-exporter" {
+  metadata {
+    name = "kube-event-exporter"
+  }
+}
+
+# https://github.com/opsgenie/kubernetes-event-exporter
+resource "helm_release" "kube-event-exporter" {
+  name = "kubernetes-event-exporter"
+  chart = "kubernetes-event-exporter"
+  repository = "https://charts.bitnami.com/bitnami"
+  version = "3.2.15"
+  namespace = kubernetes_namespace.kube-event-exporter.metadata[0].name
+
+  values = [
+    <<EOF
+config:
+  route:
+    routes:
+      - match:
+          - receiver: "elasicsearch"
+    logFormat: pretty
+  receivers:
+  - name: "elasicsearch"
+    elasticsearch:
+      hosts:
+        - "https://es${local.dev_domain_name}"
+      index: kube-events
+      indexFormat: "kube-events-{2006-01-02}"
+      username: "${jsondecode(data.aws_secretsmanager_secret_version.this.secret_string)["elasticsearch"]["username"]}"
+      password: "${jsondecode(data.aws_secretsmanager_secret_version.this.secret_string)["elasticsearch"]["password"]}"
+      deDot: true
+  EOF
+  ]
+}
+
+# # Kubernetes Event Exporter 로그에 적용할 ILM
+# resource "elasticstack_elasticsearch_index_lifecycle" "kubernetes_event_exporter" {
+#   name = "kubernetes-event-exporter"
+
+#   hot {
+#     min_age = "0ms"
+#   }
+
+#   delete {
+#     min_age = "3d"
+#     delete {}
+#   }
+# }
+
+# # Kubernetes Event Exporter 로그에 적용할 인덱스 템플릿
+# resource "elasticstack_elasticsearch_index_template" "kubernetes_event_exporter" {
+#   name = "kubernetes-event-exporter"
+
+#   index_patterns = ["*-kube-events-*"]
+#   priority       = 2000
+#   template {
+#     settings = jsonencode({
+#       "index.lifecycle.name" = elasticstack_elasticsearch_index_lifecycle.kubernetes_event_exporter.name
+#     })
+#   }
+# }
+
+resource "elasticstack_kibana_data_view" "kube-event-exporter" {
+  # for_each = local.apps
+  data_view = {
+    name            = "${local.project}-kube_event_exporter-view"
+    title           = "kube-events*"
+    time_field_name = "time"
+  }
+}
