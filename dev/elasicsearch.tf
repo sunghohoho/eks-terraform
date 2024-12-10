796bf5e2b501651466148faebabfa935baa960c1
sunghohoho <kim5259746@gmail.com>
2024-12-10 18:57:14 +0900

    add ingress index template 추가

--- dev/elasicsearch.tf
+++ dev/elasicsearch.tf
@@ -2,7 +2,44 @@ resource "elasticstack_kibana_data_view" "app" {
   # for_each = local.apps
   data_view = {
     name            = "${local.project}-view"
-    title           = "fluent*"
+    title           = "fluentbit"
     time_field_name = "time"
+  }
+}
+
+resource "elasticstack_kibana_data_view" "ingress" {
+  # for_each = local.apps
+  data_view = {
+    name            = "${local.project}-ingress-view"
+    title           = "ingress"
+    time_field_name = "time"
+  }
+}
+
+resource "elasticstack_elasticsearch_index_lifecycle" "ingress" {
+  name = "ingrss-ILM"
+
+  hot {
+    min_age = "2d"
+  }
+
+  warm {
+    min_age = "2d"
+  }
+
+  delete {
+    delete {}
+  }
+}
+
+resource "elasticstack_elasticsearch_index_template" "ingress" {
+  name = "ingress-template"
+
+  index_patterns = ["*ingress*"]
+  priority       = 1
+  template {
+    settings = jsonencode({
+      "index.lifecycle.name" = elasticstack_elasticsearch_index_lifecycle.ingress.name
+    })
   }
 }
