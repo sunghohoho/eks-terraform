0794097d721e114e92cf4df4171c436c25612750
sunghohoho <kim5259746@gmail.com>
2024-12-15 18:05:54 +0900

    Update elasicsearch.tf
    각종 index template, ilm, kibana view 추가

--- dev/elasicsearch.tf
+++ dev/elasicsearch.tf
@@ -1,17 +1,53 @@
+# https://leediz.tistory.com/38
+
+################################################################################
+# fluentbit view
+################################################################################
 resource "elasticstack_kibana_data_view" "app" {
   # for_each = local.apps
   data_view = {
-    name            = "${local.project}-view"
-    title           = "fluentbit"
+    name            = "fluentbit-view"
+    title           = "fluentbit*"
     time_field_name = "time"
   }
 }
 
+resource "elasticstack_elasticsearch_index_lifecycle" "app" {
+  name = "app-ILM"
+
+  hot {
+    min_age = "2d"
+  }
+
+  warm {
+    min_age = "2d"
+  }
+
+  delete {
+    delete {}
+  }
+}
+
+resource "elasticstack_elasticsearch_index_template" "app" {
+  name = "app-template"
+
+  index_patterns = ["fluent*"]
+  priority       = 1
+  template {
+    settings = jsonencode({
+      "index.lifecycle.name" = elasticstack_elasticsearch_index_lifecycle.app.name
+    })
+  }
+}
+
+################################################################################
+# ingress view, ILM
+################################################################################
 resource "elasticstack_kibana_data_view" "ingress" {
   # for_each = local.apps
   data_view = {
-    name            = "${local.project}-ingress-view"
-    title           = "ingress"
+    name            = "ingress-view"
+    title           = "ingress*"
     time_field_name = "time"
   }
 }
@@ -35,11 +71,91 @@ resource "elasticstack_elasticsearch_index_lifecycle" "ingress" {
 resource "elasticstack_elasticsearch_index_template" "ingress" {
   name = "ingress-template"
 
-  index_patterns = ["*ingress*"]
+  index_patterns = ["ingress*"]
   priority       = 1
   template {
     settings = jsonencode({
       "index.lifecycle.name" = elasticstack_elasticsearch_index_lifecycle.ingress.name
+    })
+  }
+}
+
+################################################################################
+# argocd, jenkins view, ILM 
+################################################################################
+resource "elasticstack_kibana_data_view" "cicd" {
+  # for_each = local.apps
+  data_view = {
+    name            = "cicd-view"
+    title           = "cicd*"
+    time_field_name = "time"
+  }
+}
+
+resource "elasticstack_elasticsearch_index_lifecycle" "cicd" {
+  name = "cicd-ILM"
+
+  hot {
+    min_age = "2d"
+  }
+
+  warm {
+    min_age = "2d"
+  }
+
+  delete {
+    delete {}
+  }
+}
+
+resource "elasticstack_elasticsearch_index_template" "cicd" {
+  name = "cicd-template"
+
+  index_patterns = ["cicd*"]
+  priority       = 1
+  template {
+    settings = jsonencode({
+      "index.lifecycle.name" = elasticstack_elasticsearch_index_lifecycle.cicd.name
+    })
+  }
+}
+
+################################################################################
+# demo app view, ILM
+################################################################################
+resource "elasticstack_kibana_data_view" "cad" {
+  # for_each = local.apps
+  data_view = {
+    name            = "cad-view"
+    title           = "default*"
+    time_field_name = "time"
+  }
+}
+
+resource "elasticstack_elasticsearch_index_lifecycle" "cad" {
+  name = "default-ILM"
+
+  hot {
+    min_age = "2d"
+  }
+
+  warm {
+    min_age = "2d"
+  }
+
+  delete {
+    delete {}
+  }
+}
+
+resource "elasticstack_elasticsearch_index_template" "cad" {
+  name = "cad-template"
+
+  index_patterns = ["default*"]
+  priority       = 1
+  template {
+    settings = jsonencode({
+      "index.lifecycle.name" = elasticstack_elasticsearch_index_lifecycle.cad.name
     })
   }
 }
