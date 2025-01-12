dce0e6dbccb153b0dd18a570eca7c8fa0d8d0ade
sunghohoho <kim5259746@gmail.com>
2025-01-12 20:19:27 +0900

    Update kubecost.tf
    kubecost on

--- modules/common/kubecost.tf
+++ modules/common/kubecost.tf
@@ -1,63 +1,63 @@
-# # prometheus 비활성화로 정상적으로 data 출력 불가
-# resource "kubernetes_namespace_v1" "kubecost" {
-#   metadata {
-#     name = "kubecost"
-#   }
-# }
+# prometheus 비활성화로 정상적으로 data 출력 불가
+resource "kubernetes_namespace_v1" "kubecost" {
+  metadata {
+    name = "kubecost"
+  }
+}
 
-# resource "kubernetes_service_account_v1" "kubecost" {
-#   metadata {
-#     name = "kubecost"
-#     namespace = kubernetes_namespace_v1.kubecost.metadata[0].name
-#   }
-# }
+resource "kubernetes_service_account_v1" "kubecost" {
+  metadata {
+    name = "kubecost"
+    namespace = kubernetes_namespace_v1.kubecost.metadata[0].name
+  }
+}
 
-# resource "aws_iam_role" "kubecost-role" {
-#   name_prefix = substr("${var.cluster_name}-kubecost-kubecost-role", 0,37)
-#   managed_policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
-#   assume_role_policy = <<POLICY
-# {
-#    "Version": "2012-10-17",
-#     "Statement": [
-#         {
-#             "Effect": "Allow",
-#             "Principal": {
-#                 "Federated": "${var.oidc_provider_arn}"
-#             },
-#             "Action": "sts:AssumeRoleWithWebIdentity",
-#             "Condition": {
-#                 "StringEquals": {
-#                     "${var.oidc_issuer_url}:aud": "sts.amazonaws.com",
-#                     "${var.oidc_issuer_url}:sub": "system:serviceaccount:kubecost:kubecost"
-#                 }
-#             }
-#         }
-#     ]
-# }
-# POLICY
-# }
+resource "aws_iam_role" "kubecost-role" {
+  name_prefix = substr("${var.cluster_name}-kubecost-kubecost-role", 0,37)
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+  assume_role_policy = <<POLICY
+{
+   "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Federated": "${var.oidc_provider_arn}"
+            },
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Condition": {
+                "StringEquals": {
+                    "${var.oidc_issuer_url}:aud": "sts.amazonaws.com",
+                    "${var.oidc_issuer_url}:sub": "system:serviceaccount:kubecost:kubecost"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
 
-# resource "helm_release" "kubecost" {
-#   chart = "cost-analyzer"
-#   name = "kubecost"
-#   repository = "https://kubecost.github.io/cost-analyzer/"
-#   namespace = kubernetes_namespace_v1.kubecost.metadata[0].name
-#   version = var.kubecost-chart-version
+resource "helm_release" "kubecost" {
+  chart = "cost-analyzer"
+  name = "kubecost"
+  repository = "https://kubecost.github.io/cost-analyzer/"
+  namespace = kubernetes_namespace_v1.kubecost.metadata[0].name
+  version = var.kubecost-chart-version
 
-#   values = [
-#     templatefile("${path.module}/helm-values/kubecost.yaml", {
-#       hostname = "kubecost${var.domain_name}"
-#       cert_arn = var.acm_arn
-#     })
-#   ]
-#   depends_on = [ 
-#     kubernetes_namespace_v1.kubecost,
-#     kubernetes_service_account_v1.kubecost,
-#     aws_iam_role.kubecost-role
-#   ]
-# }
+  values = [
+    templatefile("${path.module}/helm-values/kubecost.yaml", {
+      hostname = "kubecost${var.domain_name}"
+      cert_arn = var.acm_arn
+    })
+  ]
+  depends_on = [ 
+    kubernetes_namespace_v1.kubecost,
+    kubernetes_service_account_v1.kubecost,
+    aws_iam_role.kubecost-role
+  ]
+}
 
-#       # kubecost-sa = kubernetes_service_account_v1.kubecost.metadata[0].name
-#       # kubecost-role = aws_iam_role.kubecost-role.arn
+      # kubecost-sa = kubernetes_service_account_v1.kubecost.metadata[0].name
+      # kubecost-role = aws_iam_role.kubecost-role.arn
 
 
