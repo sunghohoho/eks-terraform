caa2b7e34b5de37868f4a868a8abc39998987034
sunghohoho <kim5259746@gmail.com>
2025-01-12 20:20:27 +0900

    add clientvpn keycloak 적용
    saml 추가

--- modules/common/keycloak.tf
+++ modules/common/keycloak.tf
@@ -154,3 +154,40 @@ resource "keycloak_openid_client" "jenkins_client" {
 
   depends_on = [ keycloak_realm.realm ]
 }
+
+################################################################################
+# cvpn
+################################################################################
+
+# AWS ClientVPN에서 사용할  Keycloak SAML Client
+resource "keycloak_saml_client" "client_vpn" {
+  realm_id  = keycloak_realm.realm.id
+  client_id = "urn:amazon:webservices:clientvpn"
+  name      = "AWS Client VPN"
+
+  client_signature_required = false
+
+  valid_redirect_uris = [
+    "http://127.0.0.1:35001",
+    "https://self-service.clientvpn.amazonaws.com/api/auth/sso/saml"
+  ]
+}
+
+# AWS ClientVPN에서 지원하지 않는 Scope 삭제 - 기본값으로 적용되는 role_list 삭제
+resource "keycloak_saml_client_default_scopes" "client_vpn" {
+  realm_id  = keycloak_realm.realm.id
+  client_id = keycloak_saml_client.client_vpn.id
+
+  default_scopes = []
+}
+
+# # XML형식으로된 SAML Client 메타데이터 다운로드 
+# # data "http" "client_vpn" {
+# #   url = "https://${data.kubernetes_ingress_v1.keycloak.spec[0].rule[0].host}/realms/${keycloak_realm.realm.realm}/protocol/saml/descriptor"
+# # }
+
+# # # SAML 제공자 생성
+# # resource "aws_iam_saml_provider" "client_vpn" {
+# #   name                   = "client-vpn"
+# #   saml_metadata_document = replace(data.http.client_vpn.response_body, "WantAuthnRequestsSigned=\"true\"", "WantAuthnRequestsSigned=\"false\"")
+# # }
